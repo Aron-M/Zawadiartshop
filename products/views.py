e4b6800f8ce30e3e42b2a01af861e5176d9cce96
Aron-M <aron.moelich88@gmail.com>
2023-08-14 10:17:59 +0000

    created model for products and migrated data. Connecte djson file using 'loaddata' command to render individual product info

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 def products(request):
     # Retrieve all products from the database
     products = Product.objects.all()
-    context = {'product': products}
+    context = {'products': products}
     return render(request, 'products.html', context)
 
 
