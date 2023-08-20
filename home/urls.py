93c7b5327b8257c017a6cec38c865e607afd9846
Aron-M <aron.moelich88@gmail.com>
2023-08-20 12:14:10 +0000

    routing all urls for navlinks in navbar

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+app_name = 'home'
 urlpatterns = [
     # URL pattern for the homepage
-    path('', views.homepage_view, name='home'),
+    path('',views.homepage_view, name='home'),
+    path('home/', views.homepage_view, name='home'),
     # Add more URL patterns for other pages as needed
 ]
--- home/views.py
+++ home/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 def homepage_view(request):
     # Render the homepage template
-    return render(request, '/workspace/Zawadiartshop/home/templates/homepage.html')
+    return render(request, 'homepage.html')
