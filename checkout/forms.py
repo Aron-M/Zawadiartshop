53c99eb51553b7c53f86d56fddf8ee6a75a25574
Aron-M <aron.moelich88@gmail.com>
2023-09-10 23:03:01 +0000

    created models and forms for checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class DeliveryAddressForm(forms.Form):
+    full_name = forms.CharField(max_length=100, label='Full Name')
+    address_line1 = forms.CharField(max_length=255, label='Address Line 1')
+    address_line2 = forms.CharField(max_length=255, required=False, label='Address Line 2')
+    city = forms.CharField(max_length=100, label='City')
+    state = forms.CharField(max_length=100, label='State/Province')
+    postal_code = forms.CharField(max_length=10, label='Postal Code')
+    country = forms.CharField(max_length=100, label='Country')
+    phone_number = forms.CharField(max_length=20, label='Phone Number')
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+# checkout/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    address_line1 = models.CharField(max_length=255)
+    address_line2 = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Shipping Address for Order #{self.order.id}"
