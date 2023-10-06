ff7c3e86efc2ca11e743f280d8a2d0e6b8e85a0f
Aron-M <aron.moelich88@gmail.com>
2023-10-06 18:49:12 +0000

    adding new line for default file storage

--- zawadiproject/settings.py
+++ zawadiproject/settings.py
@@ -161,6 +161,7 @@ CLOUDINARY_STORAGE = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
