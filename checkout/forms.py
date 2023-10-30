0e711ec0a14cb6abc028feb71109ee593b55590e
Aron-M <aron.moelich88@gmail.com>
2023-10-30 21:30:03 +0000

    styling payments tab for stripe

--- checkout/forms.py
+++ checkout/forms.py
@@ -27,7 +27,7 @@ class DeliveryAddressForm(forms.ModelForm):
             'street_address2': 'Street Address 2',
             'county': 'County',
         }
-
+        
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
@@ -37,3 +37,4 @@ class DeliveryAddressForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+            
